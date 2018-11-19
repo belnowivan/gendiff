38305e416a9776214efe6bb099c9419a995cf41e
Ivan <belnow.ivan@yandex.ru>
2018-11-20 01:01:19 +0300

    add plugin & env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
+    "parser": "babel-eslint",
     'extends': 'airbnb-base',
+    "plugins": ["babel", "jest"],
     "env": {
-       "node": true
+       "node": true,
+       "jest": true,
     },
     'rules': {
         'no-console': 0,
