5d88d9b3880fb520179a03bbfe7adcd0262ba4a6
Ivan <belnow.ivan@yandex.ru>
2018-11-20 12:58:10 +0300

    add bin for stat gendiff

---
+++ src/bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import programm from 'commander';
+
+programm
+  .version('0.1.0')
+  .option('-f', '--format [type]', 'Output format')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .parse(process.argv);
