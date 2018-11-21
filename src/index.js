0db93071d5dae1941b8f6113e6ecc8291bb62b41
Ivan <belnow.ivan@yandex.ru>
2018-11-21 17:16:26 +0300

    work

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import has from 'lodash/has';
 const diff = (dataOne, dataTwo) => {
   const arrDiff = Object.keys(dataOne)
     .reduce((acc, elem) => {
-      if (!has(dataTwo, elem)) { return [...acc, `- ${elem}: ${dataTwo[elem]}`]; }
+      if (!has(dataTwo, elem)) { return [...acc, `- ${elem}: ${dataOne[elem]}`]; }
       if (dataOne[elem] !== dataTwo[elem]) {
-        return [...acc, `- ${elem}: ${dataOne[elem]}`, `+ ${elem}: ${dataTwo[elem]}`];
+        return [...acc, `+ ${elem}: ${dataTwo[elem]}`, `- ${elem}: ${dataOne[elem]}`];
       }
-      return `  ${dataTwo[elem]}`;
-    });
+      return [...acc, `  ${dataTwo[elem]}`];
+    }, []);
   return arrDiff.join('\n');
 };
 
