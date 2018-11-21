4bc8dd041e792c3d246ef0321c74cc452dd9be73
Ivan <belnow.ivan@yandex.ru>
2018-11-21 22:30:52 +0300

    just write test

--- __test__/befoAfterJSON.test.js
+++
@@ -1,3 +0,0 @@
-import genDiff from '../src';
-
-console.log(genDiff('/home/ivan/project-lvl2-s365/__test__/__fixtures__/before_file.json', '/home/ivan/project-lvl2-s365/__test__/__fixtures__/after_file.json'));
