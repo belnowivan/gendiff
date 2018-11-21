d519895649ed08fafeea301dd2d632f5f9d6dfca
Ivan <belnow.ivan@yandex.ru>
2018-11-21 23:31:06 +0300

    ref

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,16 +1,13 @@
+/* eslint-disable lodash/prefer-lodash-method */
 import fs from 'fs';
 import union from 'lodash/union';
 import has from 'lodash/has';
 
 const diff = (dataOne, dataTwo) => {
-  // eslint-disable-next-line lodash/prefer-lodash-method
   const arrKey = union(Object.keys(dataOne), Object.keys(dataTwo));
-  // eslint-disable-next-line lodash/prefer-lodash-method
   const diffArr = arrKey.reduce((acc, key) => {
-    const icnlOneKey = has(dataOne, key);
-    const icnlTwoKey = has(dataTwo, key);
-    if (icnlOneKey && !icnlTwoKey) { return [...acc, `  - ${key}: ${dataOne[key]}`]; }
-    if (!icnlOneKey && icnlTwoKey) { return [...acc, `  + ${key}: ${dataTwo[key]}`]; }
+    if (has(dataOne, key) && !has(dataTwo, key)) { return [...acc, `  - ${key}: ${dataOne[key]}`]; }
+    if (!has(dataOne, key) && has(dataTwo, key)) { return [...acc, `  + ${key}: ${dataTwo[key]}`]; }
     return dataOne[key] !== dataTwo[key]
       ? [...acc, `  + ${key}: ${dataTwo[key]}`, `  - ${key}: ${dataOne[key]}`]
       : [...acc, `    ${key}: ${dataTwo[key]}`];
