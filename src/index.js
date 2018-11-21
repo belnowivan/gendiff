9736b7fa42028847e7b6437b42a709b215185372
Ivan <belnow.ivan@yandex.ru>
2018-11-21 21:58:50 +0300

    refact

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import fs from 'fs';
+import union from 'lodash/union';
 import has from 'lodash/has';
 
 const diff = (dataOne, dataTwo) => {
-  const arrDiff = Object.keys(dataOne)
-    .reduce((acc, elem) => {
-      if (!has(dataTwo, elem)) { return [...acc, `- ${elem}: ${dataOne[elem]}`]; }
-      if (dataOne[elem] !== dataTwo[elem]) {
-        return [...acc, `+ ${elem}: ${dataTwo[elem]}`, `- ${elem}: ${dataOne[elem]}`];
-      }
-      return [...acc, `  ${dataTwo[elem]}`];
-    }, []);
-  return arrDiff.join('\n');
+  // eslint-disable-next-line lodash/prefer-lodash-method
+  const arrKey = union(Object.keys(dataOne), Object.keys(dataTwo));
+  // eslint-disable-next-line lodash/prefer-lodash-method
+  const diffArr = arrKey.reduce((acc, key) => {
+    const icnlOneKey = has(dataOne, key);
+    const icnlTwoKey = has(dataTwo, key);
+    if (icnlOneKey && !icnlTwoKey) { return [...acc, `  - ${key}: ${dataOne[key]}`]; }
+    if (!icnlOneKey && icnlTwoKey) { return [...acc, `  + ${key}: ${dataTwo[key]}`]; }
+    return dataOne[key] !== dataTwo[key]
+      ? [...acc, `  + ${key}: ${dataTwo[key]}`, `  - ${key}: ${dataOne[key]}`]
+      : [...acc, `    ${key}: ${dataTwo[key]}`];
+  }, []);
+  return `{\n${diffArr.join('\n')}\n}`;
 };
 
 const genDiff = (firstPath, secondPath) => {
