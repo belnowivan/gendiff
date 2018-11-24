d1fa389fb5dc4c2e80e7ba593d5b3f3b552d8839
Ivan <belnow.ivan@yandex.ru>
2018-11-24 05:22:43 +0300

    s

--- src/ast.js
+++ src/ast.js
@@ -7,47 +7,29 @@ const getArrDiff = (obj1, obj2) => {
   const arrDiff = arrKey.reduce((acc, key) => {
     if (obj1[key] instanceof Object && obj2[key] instanceof Object) {
       return [...acc, {
-        children: getArrDiff(obj1[key], obj2[key]),
-        action: 'notChanged',
-        type: 'node',
-        name: key,
+        children: getArrDiff(obj1[key], obj2[key]), action: 'notChanged', type: 'node', name: key,
       }];
     }
     if (has(obj1, key) && !has(obj2, key)) {
       return [...acc, {
-        value: obj1[key],
-        action: 'dell',
-        type: 'list',
-        name: key,
+        value: obj1[key], action: 'dell', type: 'list', name: key,
       }];
     }
     if (!has(obj1, key) && has(obj2, key)) {
       return [...acc, {
-        value: obj2[key],
-        action: 'plus',
-        type: 'list',
-        name: key,
+        value: obj2[key], action: 'plus', type: 'list', name: key,
       }];
     }
     if (obj1[key] !== obj2[key]) {
       return [...acc,
         {
-          value: obj1[key],
-          action: 'dell',
-          type: 'list',
-          name: key,
+          value: obj1[key], action: 'dell', type: 'list', name: key,
         }, {
-          value: obj2[key],
-          action: 'plus',
-          type: 'list',
-          name: key,
+          value: obj2[key], action: 'plus', type: 'list', name: key,
         }];
     }
     return [...acc, {
-      value: obj1[key],
-      action: 'notChanged',
-      type: 'list',
-      name: key,
+      value: obj1[key], action: 'notChanged', type: 'list', name: key,
     }];
   }, []);
   return arrDiff;
