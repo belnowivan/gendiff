131d01391e44f5e546a1b94ca204b548f397f4eb
Ivan <belnow.ivan@yandex.ru>
2018-11-26 00:43:11 +0300

    add pretty

--- __tests__/before-after.test.js
+++ __tests__/before-after.test.js
@@ -9,7 +9,7 @@ describe('test many format', () => {
   const functTesDifftManyFormat = (extName, format) => genDiff(`${pathMinusFormatFirst}${extName}`, `${pathMinusFormatSecond}${extName}`, format);
   const expectedFlatValue = fs.readFileSync(`${expectedFlatFile}.flat.txt`, 'utf8');
   const expectedTreeValue = fs.readFileSync(`${expectedFlatFile}.tree.txt`, 'utf8');
-  // const expectedPlainValue = fs.readFileSync(`${expectedFlatFile}.plain.txt`, 'utf8');
+  const expectedPlainValue = fs.readFileSync(`${expectedFlatFile}.plain.txt`, 'utf8');
 
   test('test yml falat format', () => {
     const resultDiff = functTesDifftManyFormat('.yml', 'pretty');
@@ -39,10 +39,8 @@ describe('test many format', () => {
     const resultDiff = functTesDifftManyFormat('.tree.ini', 'pretty');
     expect(resultDiff).toBe(expectedTreeValue);
   });
-  /*
   test('test ini tree format diff plain', () => {
     const resultDiff = functTesDifftManyFormat('.tree.ini', 'plain');
     expect(resultDiff).toBe(expectedPlainValue);
   });
-  */
 });
--- src/renders/plain.render.js
+++ src/renders/plain.render.js
@@ -1,1 +1,27 @@
+/* eslint-disable lodash/prefer-lodash-method */
+const strByType = {
+  delete: ' was removed',
+  added: ' was added with value ',
+  modifed: ' was updated. From ',
+};
 
+const render = (arr) => {
+  const arrResult = arr.reduce((acc, obj) => {
+    if (obj.type === 'node') {
+      return [...acc, `Property ${obj.key}.${render(obj.children)}`];
+    }
+    if (obj.type === 'modifed') {
+      const oldVal = obj.oldValue instanceof Object ? '[complex value]' : obj.oldValue;
+      const newVal = obj.newValue instanceof Object ? '[complex value]' : obj.newValue;
+      return [...acc, `Property ${obj.key} ${strByType[obj.type]}${oldVal} to ${newVal}`];
+    }
+    if (obj.type === 'added') {
+      const newVal = obj.value instanceof Object ? '[complex value]' : obj.value;
+      return [...acc, `Property ${obj.key}${strByType[obj.type]}${newVal}`];
+    }
+    return [...acc, `Property ${obj.key}${strByType[obj.type]}`];
+  });
+  return arrResult.join('');
+};
+
+export default arr => render(arr).join('\n');
