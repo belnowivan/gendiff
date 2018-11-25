3a8a4e679a7d13023cbe0e0f6e2f75335448f6f6
Ivan <belnow.ivan@yandex.ru>
2018-11-25 05:23:32 +0300

    omg

--- src/ast.js
+++ src/ast.js
@@ -2,39 +2,34 @@
 import union from 'lodash/union';
 import has from 'lodash/has';
 
-const flattObj = obj => ((obj instanceof Object)
-  ? Object.keys(obj).map(key => ({ [key]: flattObj(obj[key]) })) : obj);
-
 const getArrDiff = (obj1, obj2) => {
   const arrKey = union(Object.keys(obj1), Object.keys(obj2));
   const arrDiff = arrKey.reduce((acc, key) => {
     if (obj1[key] instanceof Object && obj2[key] instanceof Object) {
       return [...acc, {
-        value: getArrDiff(obj1[key], obj2[key]), action: 'notChanged', type: 'node', name: key,
+        child: getArrDiff(obj1[key], obj2[key]), type: 'node', name: key,
       }];
     }
     if (has(obj1, key) && !has(obj2, key)) {
-      const type = obj1[key] instanceof Object ? 'node' : 'list';
       return [...acc, {
-        value: flattObj(obj1[key]), action: 'dell', type, name: key,
+        value: obj1[key], action: 'dell', name: key,
       }];
     }
     if (!has(obj1, key) && has(obj2, key)) {
-      const type = obj2[key] instanceof Object ? 'node' : 'list';
       return [...acc, {
-        value: flattObj(obj2[key]), action: 'plus', type, name: key,
+        value: obj2[key], action: 'plus', name: key,
       }];
     }
     if (obj1[key] !== obj2[key]) {
       return [...acc,
         {
-          value: flattObj(obj1[key]), action: 'dell', type: 'list', name: key,
+          value: obj1[key], action: 'dell', name: key,
         }, {
-          value: flattObj(obj2[key]), action: 'plus', type: 'list', name: key,
+          value: obj2[key], action: 'plus', name: key,
         }];
     }
     return [...acc, {
-      value: flattObj(obj1[key]), action: 'notChanged', type: 'list', name: key,
+      value: obj1[key], action: 'notChanged', name: key,
     }];
   }, []);
   return arrDiff;
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,22 +1,35 @@
 /* eslint-disable lodash/prefer-lodash-method */
 import path from 'path';
 import fs from 'fs';
-import flatten from 'lodash/flatten';
 import arrDiff from './ast';
 import toJsFormat from './parsers';
 
-const objmethod = {
-  plus: '+',
-  dell: '-',
-  notChanged: ' ',
+const actions = {
+  plus: '+ ',
+  dell: '- ',
+  notChanged: '  ',
 };
 
-const prettyDif = (arr, depth = 0) => {
-  const res = arr.reduce((acc, obj) => {
-    if(obj.type === node) {
-      
+const strifyObj = (elem, depth = 2, prefix = ' ') => {
+  const arrStr = Object.keys(elem).reduce((acc, key) => {
+    if (elem[key] instanceof Object) {
+      return [...acc, `${' '.repeat(depth)}${prefix}${key}: {`,
+        `${strifyObj(elem[key], depth + 4)}\n${' '.repeat(depth + 2)}}`];
     }
-  });
+    return [...acc, `${' '.repeat(depth)}${prefix}${key}: ${elem[key]}`];
+  }, []);
+  return arrStr.join('\n');
+};
+
+const prettyDif = (arr, depth = 4) => {
+  const res = arr.reduce((acc, obj) => {
+    if (obj.type === 'node') {
+      return [...acc, `  ${' '.repeat(depth)}${obj.name} : {`, ...prettyDif(obj.child, depth + 2), `  ${' '.repeat(depth)}}`];
+    }
+    const prefix = actions[obj.action];
+    const objContent = { [obj.name]: obj.value };
+    return [...acc, strifyObj(objContent, depth, prefix)];
+  }, []);
   return res;
 };
 
@@ -76,6 +89,4 @@ const a = arrDiff({
   }
 });
 
-console.log(a);
-
-console.log(prettyDif(a));
+console.log(prettyDif(a).join('\n'));
