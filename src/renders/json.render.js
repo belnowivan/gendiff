7e5465add9819dfffdb4f7ea84d0c06add492ace
Ivan <belnow.ivan@yandex.ru>
2018-11-26 08:30:41 +0300

    go soon pls

--- src/renders/json.render.js
+++ src/renders/json.render.js
@@ -1,1 +1,2 @@
-export default arrDif => JSON.stringify(arrDif, null, '  ');
+/* eslint-disable lodash/prefer-lodash-method */
+export default arrDif => arrDif.map(e => JSON.stringify(e, null, '  ')).join('\n');
