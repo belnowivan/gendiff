e7461bd6a6bedb19b37015d90ee2774b45a40974
Ivan <belnow.ivan@yandex.ru>
2018-11-26 05:01:20 +0300

    plsin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
-import formatDiff from '..';
+import getDiff from '..';
 import programm from 'commander';
 
 programm
   .version('0.1.0')
-  .option('-f', '--format [type]', 'Output format')
+  .option('-f', '--format [type]', 'Output format', 'tree')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .parse(process.argv)
-  .action((firstPath, secondPath, format) => {
-    const getFormat = formatDiff(format);
-    return console.log(getFormat(firstPath, secondPath));
-  });
+  .action((firstPath, secondPath, format) => console.log(getDiff(firstPath, secondPath, format)));
 
 programm.parse(process.argv);
--- src/genDiff.js
+++ src/genDiff.js
@@ -3,7 +3,7 @@ import path from 'path';
 import fs from 'fs';
 import arrDiff from './ast';
 import pars from './parsers';
-import getFormat from './renders';
+import getRenderToFormat from './renders';
 
 export default (firstPath, secondPath, format) => {
   const readFirstFile = fs.readFileSync(firstPath, 'utf8');
@@ -11,6 +11,6 @@ export default (firstPath, secondPath, format) => {
   const dataFirstFile = pars(readFirstFile, path.extname(firstPath));
   const dataSecondtFile = pars(readSecondFile, path.extname(secondPath));
   const diff = arrDiff(dataFirstFile, dataSecondtFile);
-  const formatOutput = getFormat(format);
+  const formatOutput = getRenderToFormat(format);
   return formatOutput(diff);
 };
